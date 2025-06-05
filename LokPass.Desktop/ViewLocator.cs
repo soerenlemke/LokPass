0041417631e25ac734175bf1457174bf2fe00f45
soerenlemke <soerenlemke@gmx.de>
2025-06-05 22:34:25 +0200

    Avalonia MVVM template project added

---
+++ LokPass.Desktop/ViewLocator.cs
@@ -0,0 +1,29 @@
+using System;
+using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using LokPass.Desktop.ViewModels;
+
+namespace LokPass.Desktop;
+public class ViewLocator : IDataTemplate
+{
+    public Control? Build(object? param)
+    {
+        if (param is null)
+            return null;
+
+        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+        var type = Type.GetType(name);
+
+        if (type != null)
+        {
+            return (Control)Activator.CreateInstance(type)!;
+        }
+
+        return new TextBlock { Text = "Not Found: " + name };
+    }
+
+    public bool Match(object? data)
+    {
+        return data is ViewModelBase;
+    }
+}
---
+++ LokPass.Desktop/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace LokPass.Desktop.ViewModels;
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}

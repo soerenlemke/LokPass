44db6cd9d715fbc9f96d8f5e6c3d684ebd5a89aa
soerenlemke <soerenlemke@gmx.de>
2025-06-09 11:42:36 +0200

    menu added with settings tab

--- LokPass.Desktop/ViewModels/MainWindowViewModel.cs
+++ LokPass.Desktop/ViewModels/MainWindowViewModel.cs
@@ -6,32 +6,30 @@ using LokPass.Core;
 using LokPass.Desktop.Models;
 
 namespace LokPass.Desktop.ViewModels;
+
 public partial class MainWindowViewModel : ViewModelBase
 {
     private readonly PasswordHasher _passwordHasher = new();
-    
-    [ObservableProperty]
-    private ObservableCollection<UserPassword> _userPasswords = [];
-    
-    [ObservableProperty]
-    private string _newTitle = "";
 
-    [ObservableProperty]
-    private string _newUsername = "";
+    [ObservableProperty] private string _newPassword = "";
 
-    [ObservableProperty]
-    private string _newPassword = "";
+    [ObservableProperty] private string _newTitle = "";
+
+    [ObservableProperty] private string _newUsername = "";
+
+    [ObservableProperty] private ObservableCollection<UserPassword> _userPasswords = [];
 
     [RelayCommand]
     private void AddUserPassword()
     {
         if (string.IsNullOrWhiteSpace(NewTitle)) return;
-        
+
         var hashedPassword = _passwordHasher.HashPassword(NewPassword);
         var parts = hashedPassword.Split(':');
-            
+
         UserPasswords.Add(
-            new UserPassword(NewTitle, NewUsername, parts[0], parts[1])
+            new UserPassword(
+                NewTitle, NewUsername, parts[0], parts[1])
         );
 
         NewTitle = "";
@@ -39,5 +37,11 @@ public partial class MainWindowViewModel : ViewModelBase
         NewPassword = "";
 
         OnPropertyChanged(nameof(UserPasswords));
+    }
+
+    [RelayCommand]
+    private void SettingsButton()
+    {
+        Console.WriteLine("ClickSettingsButton");
     }
 }

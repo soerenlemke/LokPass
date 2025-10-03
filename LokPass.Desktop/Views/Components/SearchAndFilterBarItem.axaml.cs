c585a5fa27b0c6a5e979312a8d02c1502d771c5b
soerenlemke <61249756+soerenlemke@users.noreply.github.com>
2025-10-03 14:47:20 +0200

    Refactor: Extract SearchAndFilterBar into a reusable component

---
+++ LokPass.Desktop/Views/Components/SearchAndFilterBarItem.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LokPass.Desktop.Views.Components;
+
+public partial class SearchAndFilterBarItem : UserControl
+{
+    public SearchAndFilterBarItem()
+    {
+        InitializeComponent();
+    }
+}
